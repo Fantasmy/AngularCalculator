b36dcbad6af9447523baa0fc85739701a4b004db
Jenny <fantazmit@gmail.com>
2018-08-08 21:20:26 +0200

    Basic calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+// angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
+// pages
 import { AppComponent } from './app.component';
+
+//components
+import { CalcuComponent } from './calcu/calcu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calcu/calcu.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abe-calcu',
+  templateUrl: './calcu.component.html',
+  styleUrls: ['./calcu.component.scss']
+})
+export class CalcuComponent implements OnInit {
+
+  public field1 :number;
+  public field2 :number;
+  public result :number;
+
+  sum()
+  {
+    this.result = this.field1 + this.field2;
+  }
+
+  substract()
+  {
+    this.result = this.field1 - this.field2;
+  }
+
+  multiply()
+  {
+    this.result = this.field1 * this.field2;
+  }
+
+  divide()
+  {
+    this.result = this.field1 / this.field2;
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
